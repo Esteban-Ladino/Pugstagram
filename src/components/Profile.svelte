3dba7f59f5067a770ae21a1c8c286fec73c72679
Esteban Ladino <esteban.ladino.927@gmail.com>
2020-05-24 12:10:55 -0500

    project created

---
+++ src/components/Profile.svelte
@@ -0,0 +1,41 @@
+<script>
+  export let nickname = 'esteban';
+  export let name;
+</script>
+
+<style>
+  .Profile-content {
+    display: flex;
+    align-items: center;
+  }
+  .Profile-avatar img {
+    width: 42px;
+    height: 42px;
+    border-radius: 50%;
+  }
+  .Profile-info {
+    margin: 0 0 0 0.5em;
+  }
+  .Profile-info h2 {
+    font-size: 14px;
+    color: black;
+    margin: 0;
+    padding: 0;
+  }
+  .Profile-info span {
+    font-size: 12px;
+    font-weight: normal;
+  }
+</style>
+
+<div class="Profile">
+  <div class="Profile-content">
+    <div class="Profile-avatar">
+      <img src="https://avatars3.githubusercontent.com/u/47506498?s=460&u=a5f0aad57b026ff2476e9c1ab0c7ad75f364f0c1&v=4" alt="">
+    </div>
+    <div class="Profile-info">
+      <h2>{nickname}</h2>
+      <span>{name}</span>
+    </div>
+  </div>
+</div>
